a0ae942fdba76983d853b011e4fc803dbe7687eb
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2024-11-01 12:56:11 +0000

    add logo

--- src/app/components/NavbarLanding.jsx
+++ src/app/components/NavbarLanding.jsx
@@ -24,7 +24,7 @@ const NavbarLanding = () => {
 				{/* Logo and Name */}
 				<div className="flex items-center">
 					<Image
-						src="/images/blockridelogo.png"
+						src="/images/blockridelogo.svg"
 						alt="Blockride Logo"
 						width={40}
 						height={40}
