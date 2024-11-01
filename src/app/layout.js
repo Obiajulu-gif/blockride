dbfa69d1627c93c3a3e9c530059cc7e01db40027
Emmanuel Obiajulu Okoye <64269783+Obiajulu-gif@users.noreply.github.com>
2024-11-01 12:57:45 +0000

    add logo

--- src/app/layout.js
+++ src/app/layout.js
@@ -8,6 +8,10 @@ import { ThirdwebProvider } from "thirdweb/react";
 export const metadata = {
   title: "BlockRide",
   description: "The decentralized transport system on Lisk Blockchain",
+  icons: {
+    icon: "/images/blockridelogo.svg", // 32x32 or 48x48 icon for the browser tab
+    
+  },
 };
 
 export default function RootLayout({ children }) {
